a55b77e364bd5c5bacbab02b3362d43460f68b18
Jacob Augustin <jacob.augustin@ktm.com>
2025-08-11 15:30:12 -0700

    creatued feature folder and file

---
+++ pyscripts/Feature/myfeature.py
@@ -0,0 +1,12 @@
+def testing(x , y):
+    z = 0
+    
+    z = x- y
+    if z < 0:
+        print("This is a negative number")
+    else: 
+        print("This is a positive number")
+    
+
+
+testing(4 ,5)    
